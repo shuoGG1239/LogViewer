27f24bfebf72c0fdc673531041a62d448ac11ac4
shuoGG <328893769@qq.com>
2018-06-29 20:33:27 +0800

    test

--- main.py
+++ main.py
@@ -1,54 +1,15 @@
 import sys
-import os
 
-from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
-from qss_ui_theme import green_theme
-from qss_ui_theme import qss_setting
-from qss_ui_theme import window_titlebar
+from QCandyUi import CandyWindow
 
-import LogViewer
-
-
-def scan_icon(ico_name):
-    """
-    扫描ico并返回Qicon
-    :param ico_name:
-    :return: Qicon
-    """
-    if os.path.isfile(window_titlebar.imageroot + ico_name):
-        return QIcon(window_titlebar.imageroot + ico_name)
-    else:
-        return QIcon(ico_name)
+from LogViewer import LogViewer
 
 
 def run_with_titlebar():
     app = QApplication(sys.argv)
-    keywidget = LogViewer.LogViewer()
-    mainWindow = window_titlebar.WindowWithTitleBar(keywidget, qss_setting.DARKBLUEGREEN, 0)
-    mainWindow.setWindowTitle('Log Viewer')
-    mainWindow.setWindowIcon(scan_icon('myicon.ico'))
-    green_theme.setAppGreenStyle()
-    mainWindow.show()
-    sys.exit(app.exec_())
-
-
-def run_only_greentheme():
-    app = QApplication(sys.argv)
-    mainWindow = LogViewer.LogViewer()
-    mainWindow.setWindowTitle('Log Viewer')
-    mainWindow.setWindowIcon(scan_icon('myicon.ico'))
-    green_theme.setAppGreenStyle()
-    mainWindow.show()
-    sys.exit(app.exec_())
-
-
-def run():
-    app = QApplication(sys.argv)
-    mainWindow = LogViewer.LogViewer()
-    mainWindow.setWindowTitle('Log Viewer')
-    mainWindow.setWindowIcon(scan_icon('myicon.ico'))
-    mainWindow.show()
+    w = CandyWindow.createWindow(LogViewer(), 'Log Viewer', 'myicon.ico')
+    w.show()
     sys.exit(app.exec_())
 
 
--- test.py
+++ test.py
@@ -1,6 +1,42 @@
-import re
-import color_util
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
 
-text = 'at com.simba.framework.util.jdbc.Jdbc.query(Jdbc.java:133) ~[simba-database-2.1.0-SNAPSHOT.jar!/:2.1.0-SNAPSHOT]'
-text = re.sub(r'\(([\w_]+\.java:\d+)\)', color_util.underline('\\1'), text)
-print(text)
+    def x(self):
+        return self.__x
+
+    def y(self):
+        return self.__y
+
+    def setX(self, x):
+        self.__x = x
+
+    def setY(self, y):
+        self.__y = y
+
+    def __repr__(self):
+        return 'PointId:' + str(id(self)) + ' ;X_Id:' + str(id(self.__x)) + ' ;Y_Id:' + str(id(self.__y))
+
+
+"""
+Run Result:
+    r1==> PointId:33393632 ;X_Id:1536014000 ;Y_Id:1536014032
+    r2==> PointId:33393688 ;X_Id:1536014000 ;Y_Id:1536014032
+    r3==> PointId:33393744 ;X_Id:1536014000 ;Y_Id:1536014032
+    r1==> PointId:33393632 ;X_Id:1536014064 ;Y_Id:1536014032
+    r2==> PointId:33393688 ;X_Id:1536014000 ;Y_Id:1536014032
+    r3==> PointId:33393744 ;X_Id:1536014000 ;Y_Id:1536014032
+"""
+
+if __name__ == '__main__':
+    r1 = Point(1, 2)
+    r2 = Point(1, 2)
+    r3 = Point(r1.x(), r1.y())
+    print("r1==>", r1)
+    print("r2==>", r2)
+    print("r3==>", r3)
+    r1.setX(3)
+    print("r1==>", r1)
+    print("r2==>", r2)
+    print("r3==>", r3)
